e34c41a999398c700ec37fb20a5ffa59f5d3efc4
veyndan <veyndan@gmail.com>
2019-03-26 15:26:37 +0000

    Normal distributed preferences but significant number of values are 1.0 or 0.01 due to how normal distribution works

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,28 @@
-
 import org.apache.commons.math3.distribution.NormalDistribution
 import java.util.concurrent.TimeUnit
+import kotlin.math.abs
 import kotlin.math.min
 
-data class Competitor(val name: String, val tmp: ClosedFloatingPointRange<Double>, val preferences: List<Competitor.Preference>) {
+data class Competitor(val tmp: ClosedFloatingPointRange<Double>, val preferences: List<Competitor.Preference>) {
 
     private fun body(): Double =
-        random.nextDouble(tmp.start, tmp.endInclusive).also(::println)
+        random.nextDouble(tmp.start, tmp.endInclusive)
 
     private fun compatibility(factors: List<Track.Factor>): Double =
-        1 - euclideanDistance(factors, preferences)
+        abs(1 - euclideanDistance(factors, preferences))
 
     fun stepSize(factors: List<Track.Factor>): Double =
         body() * compatibility(factors)
 
-    class Preference(value: Double) : Bound(value, 0.0..1.0)
+    data class Preference(override val value: Double) : Bound(value, 0.0..1.0)
 }
 
 data class Track(val length: Double, val factors: List<Track.Factor>) {
 
-    class Factor(value: Double) : Bound(value, 0.0..1.0)
+    data class Factor(override val value: Double) : Bound(value, 0.0..1.0)
 }
 
-class Race(private val track: Track, private val competitors: Set<Competitor>) {
+data class Race(val track: Track, private val competitors: List<Competitor>) {
 
     fun positions(competitorsDistance: Map<Competitor, Double> = competitors.associateWith { 0.0 }): Sequence<Map<Competitor, Double>> =
         sequenceOf(competitorsDistance) + sequenceOf(competitorsDistance)
@@ -34,40 +34,45 @@ class Race(private val track: Track, private val competitors: Set<Competitor>) {
             }
 }
 
-fun main() {
-    val competitors = setOf(
-        Competitor(
-            "A",
-            10.0..20.0,
-            listOf(
-                Competitor.Preference(NormalDistribution(0.3, 0.5).sample().coerceIn(0.0..1.0)),
-                Competitor.Preference(NormalDistribution(0.1, 0.5).sample().coerceIn(0.0..1.0))
+fun competitors(factorCount: Int, rangeBounds: ClosedFloatingPointRange<Double>) = generateSequence {
+    Competitor(
+        random.nextDoubleRange(rangeBounds),
+        List(factorCount) {
+            Competitor.Preference(
+                NormalDistribution(
+                    random.nextDouble(0.0, 1.0),
+                    random.nextDouble(0.0, 1.0)
+                ).sample().coerceIn(0.01..1.0)
             )
-        ),
-        Competitor(
-            "B",
-            1.0..25.0,
-            listOf(
-                Competitor.Preference(NormalDistribution(0.95, 0.5).sample().coerceIn(0.0..1.0)),
-                Competitor.Preference(NormalDistribution(0.6, 0.5).sample().coerceIn(0.0..1.0))
-            )
-        )
+        }
     )
+}
+
+fun main() {
+    val factorCount = 1000
+
+    val competitors = competitors(factorCount = factorCount, rangeBounds = 10.0..25.0).take(10)
+
+    val competitorsSample = competitors.take(3).toList()
+//    val competitorsSample = competitors.shuffled().take(random.nextInt(2, competitors.size))
 
     val race = Race(
-        Track(
-            500.0,
-            listOf(
-                Track.Factor(0.7),
-                Track.Factor(0.2)
-            )
-        ),
-//        competitors.shuffled().take(random.nextInt(2, competitors.size)).toSet()
-        competitors
+        Track(500.0, List(factorCount) { Track.Factor(random.nextDouble(0.0, 1.0)) }),
+        competitorsSample
     )
 
+    println("TRACK")
+    println(race.track)
+    println()
+
+    println("COMPETITORS")
+    competitorsSample.forEach(::println)
+    println()
+
+    println()
+
     race.positions().forEachIndexed { tick, positions ->
-//        print("\rtick=$tick ${positions.map { (competitor, position) -> "${competitor.name}=$position" }.joinToString(" ")}")
+        print("\rtick=$tick ${positions.map { (_, position) -> "$position" }.joinToString(" ")}")
 
         TimeUnit.MILLISECONDS.sleep(500L)
     }
--- src/main/kotlin/Math.kt
+++ src/main/kotlin/Math.kt
@@ -1,14 +1,34 @@
-import org.apache.commons.math3.ml.distance.EuclideanDistance
+
+import kotlin.math.pow
+import kotlin.math.sqrt
 import kotlin.random.Random
 
-open class Bound(val value: Double, private val range: ClosedFloatingPointRange<Double>) {
+open class Bound(open val value: Double, private val range: ClosedFloatingPointRange<Double>) {
 
     init {
         require(value in range) { "$value not in $range" }
     }
 }
 
-fun euclideanDistance(a: List<Bound>, b: List<Bound>): Double =
-    EuclideanDistance().compute(a.map(Bound::value).toDoubleArray(), b.map(Bound::value).toDoubleArray())
+/**
+ * Calculates the L<sub>2</sub> (Euclidean) distance between two points.
+ *
+ * @param p1 the first point
+ * @param p2 the second point
+ * @return the L<sub>2</sub> distance between the two points
+ * @throws IllegalArgumentException if the array lengths differ.
+ */
+fun euclideanDistance(p1: List<Bound>, p2: List<Bound>): Double {
+    require(p1.size == p2.size) { "${p1.size} != ${p2.size}}" }
+    return sqrt((p1 zip p2).sumByDouble { (it.first.value - it.second.value).pow(2) })
+}
 
 val random = if (DEBUG) Random(0) else Random
+
+fun Random.nextDoubleRange(range: ClosedFloatingPointRange<Double>) = nextDoubleRange(range.start, range.endInclusive)
+
+fun Random.nextDoubleRange(from: Double, until: Double): ClosedFloatingPointRange<Double> {
+    val a = nextDouble(from, until)
+    val b = nextDouble(from, until)
+    return if (a < b) a..b else b..a
+}
