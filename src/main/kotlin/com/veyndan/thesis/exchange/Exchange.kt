f831bef5339d974d0307ecf6943e4072e3f62fd1
veyndan <veyndan@gmail.com>
2019-04-09 14:31:50 +0100

    Remove database from code as not writing to it correctly

--- src/main/kotlin/com/veyndan/thesis/exchange/Exchange.kt
+++ src/main/kotlin/com/veyndan/thesis/exchange/Exchange.kt
@@ -1,9 +1,5 @@
 package com.veyndan.thesis.exchange
 
-import com.squareup.sqldelight.ColumnAdapter
-import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
-import com.veyndan.thesis.Bet
-import com.veyndan.thesis.Database
 import com.veyndan.thesis.remove
 import com.veyndan.thesis.set
 import java.math.BigDecimal
@@ -64,18 +60,6 @@ sealed class Order {
 
 class Exchange {
 
-    private val driver = JdbcSqliteDriver(name = "jdbc:sqlite:/Users/veyndan/IdeaProjects/thesis/src/main/thesis.db")
-        .also { Database.Schema.create(it) }
-    private val database = Database(driver, Bet.Adapter(
-        oddsAdapter = object : ColumnAdapter<BigDecimal, Double> {
-
-            override fun decode(databaseValue: Double): BigDecimal = databaseValue.toBigDecimal()
-
-            override fun encode(value: BigDecimal): Double = value.toDouble()
-        }
-    ))
-    private val exchangeQueries = database.exchangeQueries
-
     val backs = sortedMapOf<Odds.Back, MutableList<Order.Back>>()
     val lays = sortedMapOf<Odds.Lay, MutableList<Order.Lay>>()
 
@@ -87,31 +71,13 @@ class Exchange {
                 if (bestLay.odds.matches(order.odds)) {
                     if (order.price.value == bestLay.price.value) {
                         lays.remove(bestLay.odds, bestLay)
-                        exchangeQueries.insert(
-                            side = "Back",
-                            operation = "Matched",
-                            trader = order.trader.id,
-                            odds = order.odds.value
-                        )
                     } else if (order.price.value < bestLay.price.value) {
                         lays.getValue(bestLay.odds)[0] = bestLay.copy(price = Price(bestLay.price.value - order.price.value))
-                        exchangeQueries.insert(
-                            side = "Back",
-                            operation = "Matched",
-                            trader = order.trader.id,
-                            odds = order.odds.value
-                        )
                     } else {
                         error("Don't know what to do")
                     }
                 } else {
                     backs[order.odds] = order
-                    exchangeQueries.insert(
-                        side = "Back",
-                        operation = "Unmatched",
-                        trader = order.trader.id,
-                        odds = order.odds.value
-                    )
                 }
             }
             is Order.Lay -> {
@@ -119,20 +85,8 @@ class Exchange {
 
                 if (bestBack.odds.matches(order.odds)) {
                     backs.remove(bestBack.odds, bestBack)
-                    exchangeQueries.insert(
-                        side = "Lay",
-                        operation = "Matched",
-                        trader = order.trader.id,
-                        odds = order.odds.value
-                    )
                 } else {
                     lays[order.odds] = order
-                    exchangeQueries.insert(
-                        side = "Lay",
-                        operation = "Unmatched",
-                        trader = order.trader.id,
-                        odds = order.odds.value
-                    )
                 }
             }
         }
