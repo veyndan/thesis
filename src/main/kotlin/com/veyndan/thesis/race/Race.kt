1dc3b463e1c1138c17253485d993d9ee1d18c942
veyndan <veyndan@gmail.com>
2019-04-14 15:10:38 +0100

    Stop race after any competitor has passed the finish line

--- src/main/kotlin/com/veyndan/thesis/race/Race.kt
+++ src/main/kotlin/com/veyndan/thesis/race/Race.kt
@@ -8,7 +8,7 @@ data class Race(val track: Track, val competitors: List<Competitor>) {
 
     fun positions(competitorsDistance: Map<Competitor, Distance> = competitors.associateWith { Distance(0.0) }): Sequence<Map<Competitor, Distance>> =
         sequenceOf(competitorsDistance) + sequenceOf(competitorsDistance)
-            .takeWhile { it.any { (_, distance) -> distance.value < track.length.value } }
+            .takeWhile { it.all { (_, distance) -> distance.value < track.length.value } }
             .flatMap {
                 positions(it.mapValues { (competitor, distance) ->
                     Distance(min((distance + competitor.stepSize(track.factors)).value, track.length.value))
